11ac472596960ff1c5675634ee9fa2d07c702cd3
Reese RJ Kaz <rjkaz@comcast.net>
2021-03-30 15:41:49 -0400

    base questions

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+
+So the Answers....
+Whats the difference between undefined and null
+
+ Undefined is a varibale that has been declared but not assinged a value
+ Null is declaring the value is explicitly empty. 
+ Javascript automatically assigns no value to undefinted, so Javascript itself will never set a Null value. 
+
+*/
